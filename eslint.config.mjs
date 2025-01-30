b9f832f34ac4eb3a19b2d42bcee874c396b90a68
Nathaniel Reynolds <nathaniel.reynolds@gmail.com>
2025-01-30 13:36:16 -0500

    add strict linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,34 @@
 import nx from '@nx/eslint-plugin';
+import tseslint from 'typescript-eslint';
 
 export default [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
+
+  // Add the most strict rules provided by TS ESLint https://typescript-eslint.io/getting-started/typed-linting/
+  // NOTE: the "recommneded" rules are already included in the base Nx configs
+  ...tseslint.configs.strictTypeChecked.map((config) => ({
+    ...config,
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  })),
+  ...tseslint.configs.stylisticTypeChecked.map((config) => ({
+    ...config,
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  })),
+
   {
     ignores: ['**/dist'],
   },
